69c59a185d7ab1607809803dae524461e7f5ebf1
Darshil Shah <dks53@georgetown.edu>
2020-02-20 16:09:30 -0500

    Get latest refresh and close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,22 +4,35 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
+
+def to_usd(my_price):
+    return f"${my_price:,.2f}" #> $12,000.71
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    Source: https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/datatypes/numbers.md#formatting-as-currency
+    Param: my_price (int or float) like 4000.444444
+    Example: to_usd(4000.444444)
+    Returns: $4,000.44
+    """
 
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 
 # variables in the URL
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS") # X55IRTRY70EOOESP
-selected_ticker = input("Please enter the ticker (e.g.: AAPL) for the stock you would like to learn about: ")
+#symbol = input("Please enter the ticker (e.g.: AAPL) for the stock you would like to learn about: ")
+symbol = "MSFT"
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={selected_ticker}&interval=5min&apikey={API_KEY}"
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
 print("URL:", request_url)
 
 response = requests.get(request_url)
-print(type(response))
-print(response.status_code)
-print(type(response.text)) #>string
+#print(type(response))
+#print(response.status_code)
+#print(type(response.text)) #>string
+
 
 # handle response errors:
 if "Error Message" in response.text:
@@ -27,23 +40,29 @@ if "Error Message" in response.text:
     exit()
 
 parsed_response = json.loads(response.text)
-print(type(parsed_response)) #> dict
+#print(type(parsed_response)) #> dict
+#print(parsed_response)
 
-print(parsed_response)
+#breakpoint()
+
+latest_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = parsed_response["Time Series (Daily)"]["2020-02-20"]["4. close"]
+
+
 
 # ----------------------------------------------
 # ----------------------------------------------
 
 print("------------------------------")
-print("SELECTED SYMBOL: ", selected_ticker)
+print("SELECTED SYMBOL: ", symbol)
 print("------------------------------")
 
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("------------------------------")
 
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {latest_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("------------------------------")
